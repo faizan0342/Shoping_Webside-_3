43c5ce6cebdce92533c709f236fba88f9ac1780f
faizan <faizanhameedd0342@gmail.com>
2023-11-29 15:32:29 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import HomeScreen from "./screen/HomeScreen"
+import ProductScreen from "./screen/ProductScreen"
+import {Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="grid-container">
+    <header className="row">
+        <div className="cart.html">
+            <a href="/">Webside</a>
+        </div>
+        <div className="signin.html">
+            <a href="/signin">Sign In</a>
+            <a href="/cart">Cart</a>
+        </div>
+    </header>
+    <main>
+    <Route path="/product/:id" component={ProductScreen}></Route>
+      <Route path="/" exact component={HomeScreen}></Route>
+    </main>
+    <footer>
+        all rigth reverved 
+    </footer>
+ </div>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,49 @@
+ const product = {
+    products: [
+      {
+        _id: '1',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/img1.jpg',
+        countInStock : 5 ,
+        price: 60,
+        brand: ' Nike',
+        rating: 4.5,
+        numReviews: 10
+      },
+      {
+        _id: '2',
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '/images/img1.jpg',
+        price: 50,
+        countInStock : 10,
+        brand: ' Nike',
+        rating: 4.2,
+        numReviews: 5
+      },
+      {
+        _id: '3',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: '/images/img1.jpg',
+        price: 70,
+        brand: ' Nike',
+        countInStock : 5,
+        rating: 4.5,
+        numReviews: 8
+      }, {
+        _id: '4',
+        name: 'Best Pants',
+        category: 'Pants',
+        image: './images/img1.jpg',
+        price: 70,
+        brand: ' Nike',
+        countInStock : 0,
+        rating: 4.5,
+        numReviews: 8
+      },
+    ]
+  }
+
+  export default product
